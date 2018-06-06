a9374631e12d01a8c2ff405f144cda79b907901c
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-06 14:50:04 -0400

    adding more to the mined_minds_kata

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -14,18 +14,19 @@
 # num = num + 1
 # end
 
-num = 1
+num = 1 # giving value to a local variable
 
-100.times do
-    if num %3 == 0
+100.times do 
+    if num %15 == 0  #if number is evenly divisible by 15 put mined mind
+        puts "mined minds"
+    elsif num %3 == 0 # if a number is evenly divisible by 3, puts mined
         puts "mined"
-    elsif
-        num == 5
+    elsif num %5 == 0 # if a number is evenly divisible by 5 then puts minds
         puts "minds"
-    else
+    else # else puts num
         puts num
-    end
-num = num + 1
-end
+    end # the opposite of beginning 
+num += 1
+end # end 
 
 
