681ce030bbbb8426e7c164754413c3477663a03b
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-06 12:13:06 -0400

    first commit

---
+++ print_to_screen.rb
@@ -0,0 +1,28 @@
+# p 1 
+# puts 1
+# print 1
+
+# 100.times do
+#     puts 1
+# end
+
+# 
+# num = 1
+
+# 100.times do 
+#     puts num
+# num = num + 1
+# end
+
+num = 1
+
+100.times do
+    if num == 3
+        puts "mind"
+    else 
+        puts num
+    end
+num = num + 1
+end
+
+
